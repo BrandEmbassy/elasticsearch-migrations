3f8b6dd947080c29efab5b8067b8d637b208340b
dominikvoda <d.voda94@gmail.com>
2020-01-27 15:24:30 +0100

    @ migration logs

--- src/Migration/MigrationExecutor.php
+++ src/Migration/MigrationExecutor.php
@@ -8,6 +8,9 @@ use BrandEmbassy\ElasticSearchMigrations\Migration\Definition\Migration;
 use BrandEmbassy\ElasticSearchMigrations\Migration\Definition\MigrationsLoader;
 use Doctrine\Common\Collections\Collection;
 use Elastica\Client;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use function sprintf;
 
 final class MigrationExecutor
 {
@@ -21,13 +24,20 @@ final class MigrationExecutor
      */
     private $indexMappingPartialUpdaterFactory;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
 
     public function __construct(
         MigrationsLoader $migrationsLoader,
-        IndexMappingPartialUpdaterFactory $indexMappingPartialUpdaterFactory
+        IndexMappingPartialUpdaterFactory $indexMappingPartialUpdaterFactory,
+        ?LoggerInterface $logger = null
     ) {
         $this->migrationsLoader = $migrationsLoader;
         $this->indexMappingPartialUpdaterFactory = $indexMappingPartialUpdaterFactory;
+        $this->logger = $logger ?? new NullLogger();
     }
 
 
@@ -44,17 +54,35 @@ final class MigrationExecutor
         foreach ($migrations as $migration) {
             $indexName = $indexNameResolver->getIndexName($migration, $indexType);
 
-            $indexMappingPartialUpdater = $this->indexMappingPartialUpdaterFactory->create(
-                $elasticSearchClient,
-                $indexName
-            );
-
-            $indexMappingPartialUpdater->update($migration, $lastMigratedVersion);
+            $this->executeMigration($elasticSearchClient, $migration, $indexName, $lastMigratedVersion);
 
             $lastMigratedVersion = $migration->getVersion();
         }
 
         return $lastMigratedVersion;
+    }
+
+
+    private function executeMigration(
+        Client $elasticSearchClient,
+        Migration $migration,
+        string $indexName,
+        ?int $lastMigratedVersion
+    ): void {
+        $this->logger->info(
+            sprintf('%s index mapping migration started, current version %d', $indexName, $lastMigratedVersion ?? 0)
+        );
+
+        $indexMappingPartialUpdater = $this->indexMappingPartialUpdaterFactory->create(
+            $elasticSearchClient,
+            $indexName
+        );
+
+        $indexMappingPartialUpdater->update($migration, $lastMigratedVersion);
+
+        $this->logger->info(
+            sprintf('%s index mapping migration done, current version %d', $indexName, $migration->getVersion())
+        );
     }
 
 
